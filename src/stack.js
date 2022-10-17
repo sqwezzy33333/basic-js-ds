5c8ee16917a1ca34d72a1f16a8fb0409be5fb151
Sqwezzy33333 <sergey.kornev3@yandex.by>
2022-10-17 16:21:57 +0300

    js

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   stack = [];
+
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.stack[this.stack.length - 1];
   }
 }
 
